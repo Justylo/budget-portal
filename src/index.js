a382d45b9bba40b3a273ed7a6363cc6dfd1c5168
Justylo <justlyjohn198@gmail.com>
2022-03-07 19:13:34 +0100

    Removed unsed dependency

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-// import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
